1b110e899c8dfb736b2d308184f9a168cbf458b8
huangweiqi <askviky2010@gmail.com>
2019-05-02 01:00:44 +0800

    新增启动页业务逻辑

--- example/src/main/java/com/diabin/fastec/example/ExampleActivity.java
+++ example/src/main/java/com/diabin/fastec/example/ExampleActivity.java
@@ -1,12 +1,28 @@
 package com.diabin.fastec.example;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+
 import com.diabin.latte.activity.ProxyActivity;
 import com.diabin.latte.delegates.LatteDelegate;
+import com.diabin.latte.ec.launcher.LauncherDelegate;
 
 public class ExampleActivity extends ProxyActivity {
 
     @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.hide();
+        }
+    }
+
+    @Override
     public LatteDelegate setRootDelegate() {
-        return new ExampleDelegate();
+        //return new ExampleDelegate();
+        return new LauncherDelegate();
     }
 }
